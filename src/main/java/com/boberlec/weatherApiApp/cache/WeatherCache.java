f9f4915b6c4f22aa71dceaa3a3997df427fc18a2
boeani05 <flyingdoge@gmx.at>
2026-03-05 16:58:38 +0100

    creation of weathercache (this class will use Redis)

---
+++ src/main/java/com/boberlec/weatherApiApp/cache/WeatherCache.java
@@ -0,0 +1,13 @@
+package com.boberlec.weatherApiApp.cache;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+
+public class WeatherCache {
+
+
+}
