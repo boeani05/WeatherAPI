1867618b0fe936cfbf64898dc4ece47a7bc95d3e
boeani05 <flyingdoge@gmx.at>
2026-03-05 16:59:48 +0100

    contains main method

---
+++ src/main/java/com/boberlec/weatherApiApp/WeatherAPIApplication.java
@@ -0,0 +1,11 @@
+package com.boberlec.weatherApiApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAPIApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAPIApplication.class, args);
+    }
+}
