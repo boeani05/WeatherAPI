fcd1deb37ec806e7e267433f382a079595212e75
boeani05 <flyingdoge@gmx.at>
2026-03-11 16:41:50 +0100

    configuration for external weather API integration

---
+++ src/main/java/com/boberlec/weatherApiApp/client/WeatherApiClient.java
@@ -0,0 +1,32 @@
+package com.boberlec.weatherApiApp.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.Map;
+
+// task: weatherapiclient has the task to provide classes (e.g. service) with weather data
+@Component
+public class WeatherApiClient {
+
+    // load api key from application.properties
+    @Value("${weather.api.key}")
+    private String apiKey;
+
+    // load apiurl from application.properties
+    @Value("${weather.api.base-url}")
+    private String apiUrl;
+
+    // load a restclient from config class
+    @Autowired
+    private RestClient restClient;
+
+    public Map<String, Object> getWeather(String city) {
+        return restClient.get()
+                .uri(apiUrl + "/" + city + "?key=" + apiKey)
+                .retrieve()
+                .body(Map.class);
+    }
+}
