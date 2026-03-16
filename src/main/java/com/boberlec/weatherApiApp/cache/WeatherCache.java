ee252ae843c62103e36cf7e4e1114bb43dd272c2
boeani05 <flyingdoge@gmx.at>
2026-03-16 16:34:14 +0100

    started to work on weathercache. only implemented injection of redistemplate class

--- src/main/java/com/boberlec/weatherApiApp/cache/WeatherCache.java
+++ src/main/java/com/boberlec/weatherApiApp/cache/WeatherCache.java
@@ -1,6 +1,19 @@
 package com.boberlec.weatherApiApp.cache;
 
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class WeatherCache {
 
+    // inject class redistemplate with constructor
+    private final RedisTemplate<String, Object> redisTemplate;
 
+    public WeatherCache(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    private String buildKey(String city) {
+
+    }
 }
