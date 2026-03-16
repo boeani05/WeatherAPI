68262d525c3e3a623325d3cde03c84392161d963
boeani05 <flyingdoge@gmx.at>
2026-03-16 16:35:54 +0100

    no longer using @autowired, but using constructor

--- src/main/java/com/boberlec/weatherApiApp/controller/WeatherController.java
+++ src/main/java/com/boberlec/weatherApiApp/controller/WeatherController.java
@@ -6,14 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestController
 public class WeatherController {
 
-    @Autowired
-    private WeatherService weatherService;
+    private final WeatherService weatherService;
+
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
 
     // Add endpoint for /weather?city={city}
     @GetMapping("/weather")
