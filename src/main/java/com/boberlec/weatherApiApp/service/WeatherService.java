229b423cf74425333612a73e26002f159fd5a376
boeani05 <flyingdoge@gmx.at>
2026-03-17 16:40:35 +0100

    weather service now checks, if the weather data is stored in the cache. if so, it gets it from there, if not, it saves the data in the cache for some time

--- src/main/java/com/boberlec/weatherApiApp/service/WeatherService.java
+++ src/main/java/com/boberlec/weatherApiApp/service/WeatherService.java
@@ -1,7 +1,7 @@
 package com.boberlec.weatherApiApp.service;
 
+import com.boberlec.weatherApiApp.cache.WeatherCache;
 import com.boberlec.weatherApiApp.client.WeatherApiClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -13,33 +13,53 @@ import java.util.Optional;
 @Service
 public class WeatherService {
 
-    // di of weather api client
+    // di of weather api client/weather cache
     private final WeatherApiClient weatherApiClient;
+    private final WeatherCache weatherCache;
 
     private static final String NO_ADDRESS = "No address available!";
     private static final String NO_DESCRIPTION = "No description available!";
     private static final String NO_CONDITIONS = "No conditions available!";
     private static final String NO_TIME = "No time available!";
 
-    public WeatherService(WeatherApiClient weatherApiClient) {
+    public WeatherService(WeatherApiClient weatherApiClient, WeatherCache weatherCache) {
         this.weatherApiClient = weatherApiClient;
+        this.weatherCache = weatherCache;
     }
 
     public Map<String, Object> getWeather(String city) {
-        // calls method of weatherapiclient to get weather data (not responsibility of service)
-        Map<String, Object> weatherOfApiClientCall = weatherApiClient.getWeather(city);
-        Map<String, Object> smallerResponseOfApiClientCall = createDefaultResult(weatherOfApiClientCall);
 
-        firstDay(weatherOfApiClientCall).ifPresent(day -> {
-            smallerResponseOfApiClientCall.put("temperature", day.getOrDefault("temp", 0.0));
-            smallerResponseOfApiClientCall.put("conditions", day.getOrDefault("conditions", NO_CONDITIONS));
-            smallerResponseOfApiClientCall.put("datetime", day.getOrDefault("datetime", NO_TIME));
-        });
+        Map<String, Object> cachedWeatherData = weatherCache.getWeatherFromCache(city);
 
-        return smallerResponseOfApiClientCall;
+        // MISS case, no city found in cache
+        if (cachedWeatherData == null) {
+
+            Map<String, Object> weatherOfApiClientCall = weatherApiClient.getWeather(city);
+            Map<String, Object> smallerResponseOfApiClientCall = createDefaultResult(weatherOfApiClientCall);
+
+            // calls method of weatherapiclient to get weather data (not responsibility of service)
+            firstDay(weatherOfApiClientCall).ifPresent(day -> {
+                smallerResponseOfApiClientCall.put("temperature", day.getOrDefault("temp", 0.0));
+                smallerResponseOfApiClientCall.put("conditions", day.getOrDefault("conditions", NO_CONDITIONS));
+                smallerResponseOfApiClientCall.put("datetime", day.getOrDefault("datetime", NO_TIME));
+            });
+
+            weatherCache.saveWeatherToCache(city, smallerResponseOfApiClientCall);
+
+            return smallerResponseOfApiClientCall;
+        }
+
+        // will be put out, if city is found within cache
+        return cachedWeatherData;
+
     }
 
+    // default vanilla content of weather data (will be used, if something goes wrong)
     private Map<String, Object> createDefaultResult(Map<String, Object> weatherOfApiClientCall) {
+        if (weatherOfApiClientCall == null) {
+            weatherOfApiClientCall = Map.of();
+        }
+
         Map<String, Object> smallerResponseOfApiClientCall = new HashMap<>();
 
         smallerResponseOfApiClientCall.put("city", weatherOfApiClientCall.getOrDefault("resolvedAddress", NO_ADDRESS));
@@ -51,6 +71,7 @@ public class WeatherService {
         return smallerResponseOfApiClientCall;
     }
 
+    // puts out only the first day of the requested weather data
     @SuppressWarnings("unchecked")
     private Optional<Map<String, Object>> firstDay(Map<String, Object> weatherOfApiClientCall) {
         Object retrievedDaysCategory = weatherOfApiClientCall.get("days");
