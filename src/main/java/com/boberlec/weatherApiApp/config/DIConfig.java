78b6531fed48a5ca5403ef298596607985d235d3
boeani05 <flyingdoge@gmx.at>
2026-03-17 16:39:02 +0100

    added di for redistemplate, as i was running into a spring bean error

--- src/main/java/com/boberlec/weatherApiApp/config/DIConfig.java
+++ src/main/java/com/boberlec/weatherApiApp/config/DIConfig.java
@@ -2,6 +2,8 @@ package com.boberlec.weatherApiApp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -10,5 +12,12 @@ public class DIConfig {
     @Bean
     public RestClient returnRestClient() {
         return RestClient.builder().build();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> returnRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
     }
 }
